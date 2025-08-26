f2b6f3175e9cba16b6a47b76444842f005e71ac7
Ignatius Javelona <tobyyjavelona@gmail.com>
2025-08-27 02:32:44 +0800

    finalized pages and fixed sidebar

--- about.php
+++ about.php
@@ -1,12 +1,12 @@
 <?php
 $pageTitle = "About Me";
-$headerTitle = "Who Am I?";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title><?php echo $pageTitle; ?> - Portfolio</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -15,9 +15,13 @@ $headerTitle = "Who Am I?";
         <?php include('menu.php') ?>
 
         <div class="col-sm-9 p-4">
-            <h1><?php echo $headerTitle; ?></h1>
-            <p>I am a dedicated student with interests in web development, data engineering, and AI. I enjoy solving problems and learning new technologies.</p>
-            <img src="https://via.placeholder.com/400" class="img-thumbnail" alt="About Me Image">
+            
+            <ul>
+                <li>Github</li>
+                <li>Linkedin</li>
+                <li>Instagram</li>
+                <li>Facebook</li>
+            </ul>
         </div>
     </div>
 </div>
--- menu.php
+++ menu.php
@@ -1,24 +1,51 @@
 <?php
-// Sidebar Menu Variables
 $siteTitle = "My Student Portfolio";
 ?>
 
-<div class="col-sm-3 bg-dark text-white p-3" id="sidebar">
+
+<button class="btn btn-dark d-md-none" style="width:50px" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar">
+    ☰
+</button>
+
+<!-- Desktop -->
+<div class="col-sm-3 bg-dark text-white p-3 d-none d-md-block" id="sidebar">
     <h2 class="text-center"><?php echo $siteTitle; ?></h2>
     <ul class="nav flex-column mt-4">
         <li class="nav-item"><a href="index.php" class="nav-link text-white">Home</a></li>
         <li class="nav-item"><a href="about.php" class="nav-link text-white">About Me</a></li>
         <li class="nav-item"><a href="skills.php" class="nav-link text-white">Skills</a></li>
-        <li class="nav-item"><a href="works.php" class="nav-link text-white">Works</a></li>
+        <li class="nav-item"><a href="projects.php" class="nav-link text-white">Projects</a></li>
         <li class="nav-item"><a href="achievements.php" class="nav-link text-white">Achievements</a></li>
     </ul>
-
     <div class="mt-5 text-center">
-        <small id="date"></small>
+        <small id="D_date"></small>
+    </div>
+</div>
+
+<!-- Mobile -->
+<div class="offcanvas offcanvas-start bg-dark text-white" tabindex="-1" id="offcanvasSidebar">
+    <div class="offcanvas-header">
+        <h2 class="offcanvas-title"><?php echo $siteTitle; ?></h2>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul class="nav flex-column">
+            <li class="nav-item"><a href="index.php" class="nav-link text-white">Home</a></li>
+            <li class="nav-item"><a href="about.php" class="nav-link text-white">About Me</a></li>
+            <li class="nav-item"><a href="skills.php" class="nav-link text-white">Skills</a></li>
+            <li class="nav-item"><a href="projects.php" class="nav-link text-white">Projects</a></li>
+            <li class="nav-item"><a href="achievements.php" class="nav-link text-white">Achievements</a></li>
+        </ul>
+
+        <div class="mt-5 text-center">
+            <small id="M_date"></small>
+        </div>
     </div>
 
-    <script>
-        // Show current date
-        document.getElementById("date").innerHTML = new Date().toDateString();
-    </script>
 </div>
+
+<script>
+    document.getElementById("M_date").innerHTML = new Date().toDateString();
+    document.getElementById("D_date").innerHTML = new Date().toDateString();
+</script>
+
